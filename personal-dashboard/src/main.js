bebd8a634a920f83fb90910dae5613d5352b8130
BlaBlahBlahh <rithunyaa@gmail.com>
2026-06-28 16:48:43 +0000

    Changes

--- personal-dashboard/src/main.js
+++ personal-dashboard/src/main.js
@@ -1,18 +1,13 @@
-const { invoke } = window.__TAURI__.core;
-
-let greetInputEl;
-let greetMsgEl;
-
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
+let count = 0;
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
+  const btn = document.querySelector("#btn");
+  const status = document.querySelector("#status");
+
+  btn.addEventListener("click", () => {
+    count++;
+    status.textContent = "CLICKED " + count + " TIMES 🔥";
   });
+
+  console.log("TAURI JS LOADED ✔");
 });
